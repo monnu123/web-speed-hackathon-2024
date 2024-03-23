0a7c64eab0454e173e83e65e266357749b47e3fb
monnu123 <shimono@LAPTOP-5PFEP270.localdomain>
2024-03-23 22:19:21 +0900

    GetEpisodeListのres削減

--- workspaces/schema/src/api/episodes/GetEpisodeListResponse.ts
+++ workspaces/schema/src/api/episodes/GetEpisodeListResponse.ts
@@ -12,47 +12,10 @@ export const GetEpisodeListResponseSchema = createSelectSchema(episode)
     nameRuby: true,
   })
   .extend({
-    book: createSelectSchema(book)
-      .pick({
-        description: true,
-        id: true,
-        name: true,
-        nameRuby: true,
-      })
-      .extend({
-        author: createSelectSchema(author)
-          .pick({
-            description: true,
-            id: true,
-            name: true,
-          })
-          .extend({
-            image: createSelectSchema(image).pick({
-              alt: true,
-              id: true,
-            }),
-          }),
-        image: createSelectSchema(image).pick({
-          alt: true,
-          id: true,
-        }),
-      }),
     image: createSelectSchema(image).pick({
       alt: true,
       id: true,
     }),
-    pages: createSelectSchema(episodePage)
-      .pick({
-        id: true,
-        page: true,
-      })
-      .extend({
-        image: createSelectSchema(image).pick({
-          alt: true,
-          id: true,
-        }),
-      })
-      .array(),
   })
   .array();
 
